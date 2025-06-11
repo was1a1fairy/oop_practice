6e36e9a56e29c7a444f28c6cd0d171a30bbc49dd
Ари <arianna2811@yandex.ru>
2025-06-11 23:07:56 +0300

    feat: class book

--- main.py
+++ main.py
@@ -22,3 +22,34 @@ class Animal:
 
 cat = Animal('Милана', "кошка", 7)
 print(cat)
+print(cat.sound(input("input sound >> ")))
+
+class Book:
+
+    def __init__(
+            self,
+            title:str,
+            author: str,
+            count_pages: int,
+    ):
+        self.title = title
+        self.author = author
+        self.count_pages = count_pages
+
+    def __str__(self):
+        return (f"Название книги: {self.title}\n,"
+                f" Автор: {self.author}\n,"
+                f" Количество страниц: {self.count_pages}.")
+
+    def open(
+            self,
+            page: int
+    ):
+        if page<=self.count_pages and page>0:
+           return f"book open on page {page}"
+        else:
+            return "incorrect page"
+
+book = Book("Книга:)", "someone", 666)
+print(book)
+print(book.open(input("input page number >> ")))
