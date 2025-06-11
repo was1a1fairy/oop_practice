e5575915dc072dfdf1d72534d70e1e650f63556d
Ари <arianna2811@yandex.ru>
2025-06-11 23:51:24 +0300

    feat: class PassengerPlane

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# 1
-
 class Animal:
 
     def __init__(
@@ -37,8 +35,8 @@ class Book:
         self.count_pages = count_pages
 
     def __str__(self):
-        return (f"Название книги: {self.title}\n,"
-                f" Автор: {self.author}\n,"
+        return (f"Название книги: {self.title},\n"
+                f" Автор: {self.author},\n"
                 f" Количество страниц: {self.count_pages}.")
 
     def open(
@@ -52,4 +50,63 @@ class Book:
 
 book = Book("Книга:)", "someone", 666)
 print(book)
-print(book.open(input("input page number >> ")))
+print(book.open(int(input("input page number >> "))))
+
+class PassengerPlane:
+
+    def __init__(
+            self,
+            manufacturer: str,
+            model: str,
+            capacity: int,
+            current_height: int,
+            current_speed: int
+    ):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.capacity = capacity
+        self.current_height = current_height
+        self.current_speed = current_speed
+
+    def __str__(self):
+        return (f"Производитель самолёта: {self.manufacturer},\n"
+                f" Модель самолёта: {self.model},\n"
+                f" Вместимость пассажиров: {self.capacity},\n"
+                f" Текущая высота: {self.current_height},\n"
+                f" Текущая скорость: {self.current_speed}")
+
+    def takeoff(self, value_for_height: int, value_for_speed: int):
+        if self.current_speed==0 or self.current_height==0:
+            self.current_height += value_for_height
+            self.current_speed += value_for_speed
+            print("airplane takeoff!")
+        else:
+            print("airplane already takeoff:(((")
+
+    def landing(self):
+        if self.current_speed==0 or self.current_height==0:
+            print("airplane already landed:(((")
+        else:
+            print("airplane landed!")
+
+    def change_height(self, value: int):
+        if value>0 or value<0:
+            self.takeoff(value, 0)
+            self.current_height += value
+            print("height was changed")
+        if value==0:
+            print('height wasnt changed')
+
+    def change_speed(self, value: int):
+        if value>0 or value<0:
+            self.takeoff(0, value)
+            self.current_speed += value
+            print("speed was changed")
+        if value==0:
+            print('speed wasnt changed')
+
+airplane = PassengerPlane('someone', 'model one', 36, 0, 0)
+print(airplane.landing())
+print(airplane.takeoff(133, 200))
+print(airplane.change_speed(13))
+print(airplane.change_height(0))
